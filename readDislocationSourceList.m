ac58640b4a6afdcdf33cf0aeb5ca49d2883e9169
Adhish Majumdar <adhishm@gmail.com>
2013-04-03 11:12:02 +0200

    *readDislocationList.m Read dislocations from file *readDislocationSourceList.m Read dislocation sources from file

--- readDislocationSourceList.m
+++ readDislocationSourceList.m
@@ -1,12 +1,12 @@
-function dList = readDislocationList (filename, delimiter)
-%% dList = readDislocationList (filename, delimiter)
+function dsList = readDislocationSourceList (filename, delimiter)
+%% dsList = readDislocationSourceList (filename, delimiter)
 %  Reads from the file <filename> the list of dislocations present on the
 %  slip plane. The file data should be in the following format:
-%  b1 b2 b3 l1 l2 l3 f
+%  b1 b2 b3 l1 l2 l3 f t
 %  where [b1 b2 b3] is the Burgers vector, [l1 l2 l3] is the dislocation
-%  line vector and f is the fractional position on the slip plane. The
-%  variable delimiter provides the character used to delimit the data. The
-%  default calue of the delimiter is ' ' or space.
+%  line vector, t is the critical stress and f is the fractional position
+%  on the slip plane. The variable delimiter provides the character used to
+%  delimit the data. The default calue of the delimiter is ' ' or space.
 
     %% Read file data
     if nargin==1
@@ -18,13 +18,14 @@ function dList = readDislocationList (filename, delimiter)
     
     %% Create the dislocation list
     % Preallocate the structure
-    dList(nData) = struct('b', [0 0 0], 'l', [0 0 0], 'f', 0);
+    dsList(nData) = struct('b', [0 0 0], 'l', [0 0 0], 'f', 0);
     % Populate the list from file data
     for i=1:nData
         bvec = data(i, 1:3);
-        lvec = data(i, 4:6);
+        lvec = data(i, 4:6);        
         fpos = data(i, 7);
-        dList(i) = createDislocation(bvec, lvec, fpos);
+        tauc = data(i, 8);
+        dsList(i) = createDislocationSource(bvec, lvec, fpos, tauc);
     end
     
     %% Clear memory
