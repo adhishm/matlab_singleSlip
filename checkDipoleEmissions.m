46ec5a3efd0187a13bc89f86936058c70fe75c15
Adhish Majumdar <adhishm@gmail.com>
2013-04-03 17:39:09 +0200

    Defined functions pertaining to dipole emissions: *checkDipoleEmissions.m: Check if the resolved stress on the source is greater than the critical stress. *dipoleLength.m: Calculate the length of the dislocation dipole. *emitDipole.m: Emit a dislocation dipole, concatenate it to the list of dislocations, and re-sort the list.

---
+++ checkDipoleEmissions.m
@@ -0,0 +1,16 @@
+function flag = checkDipoleEmissions (tau, dSources)
+%% flag = checkDipoleEmissions (tau, dSources)
+%  Returns a vector of flags indicating whether each dislocation source
+%  present in the list dSources will emit a dislocation dipole or not.
+
+    %% Check for dipole emission criterion
+    
+    nSources = length(dSources);
+    flag = zeros(1, nSources);
+    
+    for i=1:nSources
+        if tau > dSources(i).t
+            flag(i) = 1;
+        end
+    end
+end
