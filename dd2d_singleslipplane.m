899b9cf11eaf53eeb3329e6f53a8be569afc9467
Adhish Majumdar <adhishm@gmail.com>
2013-04-05 11:12:09 +0200

    *initializeSimulation.m: Initialize simulation parameters *dd2d_singleslipplane.m: Simulation manager. Force calculation, velocity calculations, displacements, stopping criterion complete. Pending: dipole emissions.

--- dd2d_singleslipplane.m
+++ dd2d_singleslipplane.m
@@ -8,6 +8,60 @@ initializeSimulation;
 %% Load data from files
 dList = readDislocationList ( dislocationStructureFile );
 dSourceList = readDislocationSourceList ( dislocationSourceFile );
+nDisl = length(dList);
 
 %% Slip plane
 slipPlane = createSlipPlane (dList, dSourceList, extremities, fpos);
+slipPlaneVector = slipPlane.es(1,:) - slipPlane.es(2,:);
+normSlipPlaneVector = norm(slipPlaneVector);
+
+%% Pre-allocate and calculate dislocationPosition vector
+dislocationPosition = zeros(nDisl, 3);
+for i=1:nDisl
+    dislocationPosition(i,:) = positionVector (dList(i).f, slipPlane.es);
+end
+
+%% Iteration counters
+totalTime = 0.0;
+totalSteps = 0;
+continueSimulation = true;
+
+%% Iterate
+while continueSimulation
+    %% Forces
+    % Peach-Koehler force
+    f_PK = dislocation_PKForces(appliedStress, dList);
+    % Dislocation interaction force
+    f_d  = dislocation_interactionForce (dList, slipPlane.es, mu, nu);
+    % Total force
+    f_total = f_PK + f_d;
+    
+    %% Dislocations velocities
+    velocityList = dislocationVelocities (f_total, dragCoefficient);
+    
+    %% Move the dislocations
+    % Initial absilute position vector
+    dislocationPosition = dislocationPosition + (velocityList * timeStep) ;
+    for i=1:nDisl
+        % Calculate fractional position
+        dList(i).f = norm(dislocationPosition(i,:) - slipPlane.es(1,:))/normSlipPlaneVector;
+    end
+    % Dislocation dipole emissions
+    
+    % Time step
+    totalTime = totalTime + timeStep;    
+    % Number of iterations
+    totalSteps = totalSteps + 1;    
+    % Stopping criterion
+    if stoppingCriterion==1
+        % Number of steps
+        if totalSteps >= limitingSteps
+            continueSimulation = false;
+        end
+    else
+        % Total time
+        if totalTime >= limitingTime
+            continueSimulation = false;
+        end
+    end
+end
--- initializeSimulation.m
+++ initializeSimulation.m
@@ -6,9 +6,16 @@
 dislocationStructureFile = 'dislList.txt';
 dislocationSourceFile    = 'dsourceList.txt';
 
+%% Slip plane
+extremities = [ 0 0 0;
+                5 5 5 ];
+fpos = 0.5;
+
 %% Material properties
 dragCoefficient = 1.0;                  % Kg/s
 criticalStressDipoleEmission = 1.0;     % Pa
+mu = 1.0e9;                             % Pa
+nu = 0.33;
 
 %% Simulation parameters
 timeStep = 10.0e-09;                    % s
