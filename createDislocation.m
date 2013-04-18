004de43b1c9d4caadb6cb362823c3bbb362cd835
Adhish Majumdar <adhishm@gmail.com>
2013-04-18 15:53:09 +0200

    Added mobileFlag to createDislocation function

--- createDislocation.m
+++ createDislocation.m
@@ -1,12 +1,15 @@
-function disl = createDislocation (bvec, lvec, fpos)
-%% disl = createDislocation (bvec, lvec, fpos)
+function disl = createDislocation (bvec, lvec, fpos, mobileFlag)
+%% disl = createDislocation (bvec, lvec, fpos, mobileFlag)
 %  Returns in disl a dislocation with the attributes specified.
 %  bvec Burgers vector; 1x3 vector
 %  lvec Line vector;    1x3 vector
 %  fpos Fractional position along slip-plane; scalar
+%  mobileFlag Indiaces whether the dislocation is mobile or not; 0:
+%  blocked, 1: mobile.
 
     %% Populate data
     disl.b = bvec;
     disl.l = lvec;
     disl.f = fpos;
+    disl.m = mobileFlag;
 end
