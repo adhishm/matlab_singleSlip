ea16236c2faf2a1b13091f5784339f1e20026660
Adhish Majumdar <adhishm@gmail.com>
2013-04-18 16:50:07 +0200

    Defined calculation of minimum time increment

--- timeIncrement.m
+++ timeIncrement.m
@@ -4,3 +4,31 @@ function globalTimeIncrement = timeIncrement (dList, dSourceList, dislocationPos
 %  one dislocation approach another. Beyond this, both dislocations are
 %  pinned.
 
+    %% Find out number of dislocations
+    
+    nDisl = length (dList);
+    
+    %% Iterate dislocations
+    
+    % Distance between this dislocation and others
+    % vector (3), norm
+    distanceVectors = zeros(nDisl, 4);
+    velocityVectors = zeros(nDisl, 4);
+    
+    for i=1:nDisl
+        
+        unitV = velocityList(i,:)/norm(velocityList(i,:));  % Unit vector for velocity
+        
+        for j=1:nDisl
+            if (i~=j)
+                distanceVectors (j, 1:3) = dislocationPosition (j,:) - dislocationPosition (i,:);
+                distanceVectors (j, 4)   = norm (distanceVectors (j, 1:3));
+                velocityVectors (j, 1:3) = velocityList (j,:) - velocityList (i,:);
+                velocityVectors (j, 4)   = norm (velocityVectors (j, 1:3));
+            end
+        end
+        
+        
+    end
+    
+    
