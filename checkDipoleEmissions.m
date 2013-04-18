dfc12ccac89ab7a3da48fd48723bfa0723057540
Adhish Majumdar <adhishm@gmail.com>
2013-04-18 17:34:49 +0200

    Defined limiting turns in checkDipoleEmissions

--- checkDipoleEmissions.m
+++ checkDipoleEmissions.m
@@ -1,5 +1,5 @@
-function flag = checkDipoleEmissions (tau, dSources)
-%% flag = checkDipoleEmissions (tau, dSources)
+function flag = checkDipoleEmissions (tau, dSources, turnsDislocationSource)
+%% flag = checkDipoleEmissions (tau, dSources, turnsDislocationSource)
 %  Returns a vector of flags indicating whether each dislocation source
 %  present in the list dSources will emit a dislocation dipole or not.
 
@@ -10,7 +10,12 @@ function flag = checkDipoleEmissions (tau, dSources)
     
     for i=1:nSources
         if tau > dSources(i).t
-            flag(i) = true;
+            dSources(i).s = dSources(i).s + 1;  % Add a turn to the counter
+            if dSources(i).s == turnsDislocationSource
+                % The limiting number of turns is reached
+                flag(i) = true;
+                dSources(i).s = 0;
+            end
         else
             flag(i) = false;
         end
