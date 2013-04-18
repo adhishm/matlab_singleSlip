144c52e8074a80095d286f2b1ef2b55bce1d0acd
Adhish Majumdar <adhishm@gmail.com>
2013-04-18 15:50:10 +0200

    Created function file timeIncrement.m Added description.

--- timeIncrement.m
+++ timeIncrement.m
@@ -1,2 +1,6 @@
-function globalTimeIncrement = timeIncrement (dList, dSourceList, dislocationPosition, dSourcePositions, velocityList)
-%% 
+function globalTimeIncrement = timeIncrement (dList, dSourceList, dislocationPosition, dSourcePositions, velocityList, limitingDistance)
+%% globalTimeIncrement = timeIncrement (dList, dSourceList, dislocationPosition, dSourcePositions, velocityList, limitingDistance)
+%  Calculates the time increment based on the limiting distance up to which
+%  one dislocation approach another. Beyond this, both dislocations are
+%  pinned.
+
